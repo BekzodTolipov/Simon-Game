775b0c765a487a91007092749c970fb5fe58a2c2
BekzodTolipov <btnk@mail.umsl.edu>
2020-03-15 13:55:07 -0500

    Initial commit

---
+++ SimonSaysApp/app/src/main/java/edu/umsl/simonsaysapp/GameStatistics.kt
@@ -0,0 +1,12 @@
+package edu.umsl.simonsaysapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameStatistics : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_statistics)
+    }
+}
---
+++ SimonSaysApp/app/src/main/java/edu/umsl/simonsaysapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package edu.umsl.simonsaysapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Define needed variables
+        var random = (0..3).random()
+        val fourColors = arrayOf("Green", "Yellow", "Blue", "Red")
+        val allColors: ArrayList<String> = arrayListOf(fourColors[random])
+        val start = findViewById<Button>(R.id.startBtn)
+        val activitiesArray = arrayOf(MainGame::class.java, Yellow::class.java, Blue::class.java, Red::class.java)
+
+        // Colors Simon says
+        for(i in 0..3){
+            random = (0..3).random()
+            allColors.add(fourColors[random])
+        }
+
+        // Start button used to begin game
+        start.setOnClickListener {
+            val intent = Intent(this@MainActivity, activitiesArray[random])
+            intent.putStringArrayListExtra("colors", allColors)
+            intent.putExtra("count", 0)
+            intent.putExtra("score", 0)
+            startActivity(intent)
+        }
+    }
+}
